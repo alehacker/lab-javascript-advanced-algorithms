d349e6ce4e719b42443034aa6429cf0a2b81882d
Ale Kennedy <alepertuz@gmail.com>
2023-01-26 10:51:27 -0500

    Iteration 1 - The Stack class

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -3,27 +3,52 @@ class Stack {
     this.stackControl = [];
     this.MAX_SIZE = 10;
   }
-
+  // canPush is a boolean function that checks if the stack is full or not
   canPush() {
-    // ... your code goes here
+    let result = false
+    if (this.stackControl.length < this.MAX_SIZE){
+        result = true;
+    }
+    return result;
   }
 
+  // isEmpty is a boolean function that checks if the stack is empty
   isEmpty() {
-    // ... your code goes here
+    return this.stackControl.length === 0
   }
 
+  //push will put new items into the array and will flag stack overflow
   push(item) {
-    // ... your code goes here
+    if (this.canPush()){
+        this.stackControl.push(item)
+        return this.stackControl
+    }
+    throw new Error('STACK_OVERFLOW');
   }
 
+  //This function will remove the last element in the stack.
   pop() {
-    // ... your code goes here
-  }
+    switch(true){
+        case this.isEmpty():
+            throw new Error('STACK_UNDERFLOW');
+            break;
+        default:
+            return this.stackControl.pop();
+            break;
+    }
 
+    // if (!(this.isEmpty())) {
+    //     this.stackControl.pop();
+    // }
+    // throw new Error('STACK_UNDERFLOW');
+
+  }
+  // This function will return the list of items stored in the stack.
   display() {
-    // ... your code goes here
-  }  
-}
+    return this.stackControl
+  }
+}  
+
 
 // This is required to enable the automated tests, please ignore it.
 if (typeof module !== 'undefined') module.exports = Stack;
